574d197820bf1f3f835af934e7956b1157cb41e8
dj04304 <dj043045@gmail.com>
2022-12-12 11:38:58 +0900

    Jsp 파라미터 활용

--- src/main/java/web/servlet/api/GetUsername.java
+++ src/main/java/web/servlet/api/GetUsername.java
@@ -1,6 +1,8 @@
 package web.servlet.api;
 
 import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -21,17 +23,38 @@ public class GetUsername extends HttpServlet {
 		System.out.println("서블릿 객체 소멸 시 실행");
 	}
 	
-//주석처리하면 get요청이 실행 됨
+	
 //	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+//		//요청마다 어떤 요청인지 출력
+//		System.out.println(request.getMethod());
 //		System.out.println("서비스 호출");
 //	}
-
+	
+	@Override
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		String t = request.getParameter("test");
+		String name = request.getParameter("name");
+		
 		System.out.println("Get요청 들어옴");
 		System.out.println("Read");
+		
+		System.out.println("test: " + t);
+		System.out.println("name: " + name);
+		
+		request.setAttribute("name", name);
+		
+		RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/views/user.jsp");
+		dispatcher.forward(request, response);
+		
+		//한글로 나오기 위한 encoding
+//		response.setContentType("text/html; charset=UTF-8");
+//		response.setCharacterEncoding("UTF-8");
+//		response.getWriter().println("<html><head></head><body>");
+//		response.getWriter().println("<h1>안녕하세요</h1></body></html>");
 	}
 
-
+	@Override
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		System.out.println("Post요청 들어옴");
 		System.out.println("Create");
